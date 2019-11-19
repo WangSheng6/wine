b4e241387b4043ec8956d180888f3d905a703795
王声 <780488337@qq.com>
2019-11-19 13:18:27 +0800

    新增basename，解决相对路径地址问题

--- build/precache-manifest.4e00b0f14faeb7f05d54634c7e3ea2f3.js
+++ build/precache-manifest.4e00b0f14faeb7f05d54634c7e3ea2f3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9ee37ec75ef686a1534d69b455f835b7",
+    "revision": "cb8d821b85d2f33b42778d792793ff1c",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.5a9b341d.chunk.css"
   },
   {
-    "revision": "fa3191b2f88852aaac6c",
+    "revision": "39523c17618a425f0295",
     "url": "./static/css/main.9045d1d0.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.1e4da6d6.chunk.js"
   },
   {
-    "revision": "fa3191b2f88852aaac6c",
-    "url": "./static/js/main.02b5b812.chunk.js"
+    "revision": "39523c17618a425f0295",
+    "url": "./static/js/main.b6193c60.chunk.js"
   },
   {
     "revision": "56a6b2399647f3d207bd",
