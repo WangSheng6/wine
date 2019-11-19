0eb0cc4d0bcba762a9db39689cd928c9a72bd692
王声 <780488337@qq.com>
2019-11-19 14:16:01 +0800

    格式化代码新增basename，解决相对路径地址问题

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import './index.css';
 //import App from './home/App';
 import * as serviceWorker from './serviceWorker';
 // 路由使用 history模式
-import { BrowserRouter,Route } from 'react-router-dom';
+import { HashRouter,Route } from 'react-router-dom';
 // 路由采用 hash模式
 // import { HashRouter } from 'react-router-dom'
 import Home from './home/App'
 import List from './list/List'
 
 ReactDOM.render(
-  <BrowserRouter basename='/wine/'>
+  <HashRouter basename='/wine/'>
     <Route exact path='/' component={Home}/>
     <Route exact path='/list' component={List}/>
-  </BrowserRouter>
+  </HashRouter>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
