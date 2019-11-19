636e64d29d36a80a901f9a371748c46d9251f8c0
王声 <780488337@qq.com>
2019-11-19 14:03:26 +0800

    格式化代码新增basename，解决相对路径地址问题

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import Home from './home/App'
 import List from './list/List'
 
 ReactDOM.render(
-  <BrowserRouter basename='/wine/'>
+  <BrowserRouter basename='/wine'>
     <Route exact path='/' component={Home}/>
     <Route exact path='/list' component={List}/>
   </BrowserRouter>
