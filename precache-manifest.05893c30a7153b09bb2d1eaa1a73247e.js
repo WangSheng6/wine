3b6fc04d4d18161af1c259932c8934af69ac26ec
王声 <780488337@qq.com>
2019-11-19 17:58:11 +0800

    Updates

---
+++ precache-manifest.05893c30a7153b09bb2d1eaa1a73247e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2edf09fcec682067c063b42b4e635656",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d42ee7c9d472ffcc7418",
+    "url": "./static/css/2.3fec2a62.chunk.css"
+  },
+  {
+    "revision": "64fd308c779186d2c6c8",
+    "url": "./static/css/main.e8f80562.chunk.css"
+  },
+  {
+    "revision": "d42ee7c9d472ffcc7418",
+    "url": "./static/js/2.a02c443f.chunk.js"
+  },
+  {
+    "revision": "64fd308c779186d2c6c8",
+    "url": "./static/js/main.dd5961b6.chunk.js"
+  },
+  {
+    "revision": "56a6b2399647f3d207bd",
+    "url": "./static/js/runtime-main.f1c4a8af.js"
+  }
+]);
